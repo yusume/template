cfc53a826c5b32e3505028036ebe38c045b3e06a
yusumi <yusume@naver.com>
2019-11-08 17:14:02 +0900

    top, footer

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,38 @@
 // 상단 메뉴 고정 
 $(document).ready(function () {
-    
+
+});
+
+console.log();
+if ($(window).width() > 768) {
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            $(".navbar").css({ "padding": "0.6rem 0.6rem", "transition": "padding .2s" });
+        } else {
+            $(".navbar").css({ "padding": "2rem 2rem", "transition": "padding .2s" });
+        }
+    }
+}
+
+
+(function (global, $) {
+    var $doc = $('html, body');
+
+    var btnTop = $('.btn-top');
+    btnTop.on('click', 'a', function (e) {
+        e.preventDefault();
+        $doc.stop()
+            .animate({
+                scrollTop: 0
+            }, 800)
     });
+    var btndown = $('.btn-down');
+    btndown.on('click', 'a', function (e) {
+        e.preventDefault();
+        $doc.stop().animate({
+            scrollTop: 600
+        }, 800)
+    });
+}(window, window.jQuery));
