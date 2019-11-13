dd890888f2a89ce3cec276a31570537f514f05d0
yusumi <yusume@naver.com>
2019-11-13 09:54:33 +0900

    code 정리 및 ui 수정

--- dist/js/script.js
+++ dist/js/script.js
@@ -37,4 +37,17 @@ if ($(window).width() > 768) {
             scrollTop: 600
         }, 800)
     });
+
+    //파일 업로드
+
+    var fileTarget = $('.table .upload-hidden');
+  
+      fileTarget.on('change', function(){
+          if(window.FileReader){
+              var filename = $(this)[0].files[0].name;
+          } else {
+              var filename = $(this).val().split('/').pop().split('\\').pop();
+          }
+          $(this).siblings('.upload-name').val(filename);
+      });
 }(window, window.jQuery));
